901fb6e8fc5b6b419e32659195eff1bea67c80d3
Nimesh Solanki <nimesh3536@gmail.com>
2020-05-17 12:03:18 +0530

    add navbar and its styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Home from './Home';
+import Navbar from './Navbar';
 const App = () => {
   return (
     <div className='page-border'>
+      <Navbar />
       <Home />
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className='nav'>
+        <nav>
+          {/* <ul> */}
+            <span><a href="mailto:nimesh3536@gmail.com">Contact</a></span>
+            <span><a href="#">Portfolio</a></span>
+            <span><a href="#">Resume</a></span>
+          {/* </ul> */}
+        </nav>
+    </div>
+  );
+};
+
+export default NavBar;
