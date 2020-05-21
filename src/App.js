413d9ab7e94c94682a9392f9f6e221704f513a9b
Nimesh Solanki <nimesh3536@gmail.com>
2020-05-21 22:36:12 +0530

    add section

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import AboutMe from './AboutMe';
 
 const App = () => {
   return (
-
-     <div>
-       <div className="top-line"></div>
-       <div className="left-line" ></div> 
-       <div className="right-line"></div>
-       <div className="bottom-line"></div>
-      <div >
+    <div>
+      <div className='top-line'></div>
+      <div className='left-line'></div>
+      <div className='right-line'></div>
+      <div className='bottom-line'></div>
+      <section id='home'>
         <Navbar />
         <Home />
-      <AboutMe />
-      </div>
+      </section>
+
+      <section id='about'>
+        <AboutMe />
+      </section>
     </div>
   );
 };
