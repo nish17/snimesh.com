946673c5e5603b3235b344200dd46230a210e4d4
Nimesh Solanki <nimesh3536@gmail.com>
2020-06-03 16:50:01 +0530

    add education and experience component

---
+++ src/Education.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Education  = () => {
+  return (
+    <div>
+      <h4>Education</h4>
+      <div className="card-title" >
+        <h4>
+          Bachelor of Technology in Information and Communication Technology
+        </h4>
+      </div>
+      <div className="card-desc" >
+        <h5>Batch 2016, Pandit Deendayal Petroleum Univeristy, Gandhinagar</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
---
+++ src/Experience.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Experience = () => {
+  return (
+    <div className="fit" >
+      <h4>Experience</h4>
+      <div className='card-title'>
+        <h4>Software Developer Intern - TapChief</h4>
+      </div>
+      <div className='card-desc'>
+        <p>
+          For the major part of the internship, I worked on building the
+          onboarding flow for businesses and consultants (i.e. users). Also
+          helped the company to deploy it on Heroku
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
