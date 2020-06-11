589d5ffec8630018d67fb4bbd7c3b37410f76cfa
Nimesh Solanki <nimesh3536@gmail.com>
2020-06-12 01:31:04 +0530

    update education and experience

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,38 +7,47 @@ import Col from 'muicss/lib/react/col';
 // import Row from 'react-bootstrap/Row'
 // import Col from 'react-bootstrap/Col'
 
-import Experience from './Experience';
-import Education from './Education';
+import ExperienceAndEducation from './ExperienceAndEducation';
 import GithubCard from './GithubCard';
 const AboutMe = () => {
   return (
-    <div className='grid-container'>
-      <div class='about-title-1'>
-        <h3>About</h3>
+    <div>
+      <div className='grid-container'>
+        <div class='about-title-1'>
+          <h3>About</h3>
+        </div>
+        <div className='img'>
+          <img
+            className='my-pic-1'
+            src='https://avatars1.githubusercontent.com/u/12984120?v=4'
+          />
+        </div>
+        <div className='content-1'>
+          <p>
+            I'm a developer at heart with experience in{' '}
+            <a href='https://nodejs.org/'>Node.js</a> on the backend and{' '}
+            <a href='https://developers.google.com/assistant'>
+              Google Assistant
+            </a>{' '}
+            /<a href='https://developer.amazon.com/en-US/alexa'>Amazon Alexa</a>
+            /{' '}
+            <a href='https://www.samsung.com/us/explore/bixby/'>
+              Samsung Bixby
+            </a>{' '}
+            on the VoiceEnd. <br /> I am seeking for opportunity with a
+            competitive and challenging enivronment where I can serve your
+            organization and establish an enjoyable career for myself.
+          </p>
+        </div>
+        <div className='button-1'>
+          {/* <a href='mailto:nimesh3536@gmail.com'>Email me</a> */}
+          <a href='#Resume'>Checkout Resume</a>
+        </div>
+        <GithubCard />
       </div>
-      <div className='img'>
-        <img
-          className='my-pic-1'
-          src='https://avatars1.githubusercontent.com/u/12984120?v=4'
-        />
+      <div>
+        <ExperienceAndEducation />
       </div>
-      <div className='content-1'>
-        <p>
-          I'm a developer at heart with experience in{' '}
-          <a href='https://nodejs.org/'>Node.js</a> on the backend and{' '}
-          <a href='https://developers.google.com/assistant'>Google Assistant</a>{' '}
-          /<a href='https://developer.amazon.com/en-US/alexa'>Amazon Alexa</a>/{' '}
-          <a href='https://www.samsung.com/us/explore/bixby/'>Samsung Bixby</a>{' '}
-          on the VoiceEnd. <br /> I am seeking for opportunity with a
-          competitive and challenging enivronment where I can serve your
-          organization and establish an enjoyable career for myself.
-        </p>
-      </div>
-      <div className='button-1'>
-        {/* <a href='mailto:nimesh3536@gmail.com'>Email me</a> */}
-        <a href='#Resume'>Checkout Resume</a>
-      </div>
-      <GithubCard />
     </div>
   );
 };
