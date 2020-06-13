eb2476ee77b9913e2a6bb40e75dbc3d46f3e07e7
Nimesh Solanki <nimesh3536@gmail.com>
2020-06-14 02:24:56 +0530

    make grid container for awards

--- src/Awards.js
+++ src/Awards.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
+import VOICE from './assets/Voice.webp';
+import VoiceAwards from './assets/inside_voice_awards.png';
 const Awards = () => {
   return (
-    <div></div>
+    <div className='awards'>
+      <h3>Awards</h3>
+      <div className='awards-container'>
+        <div className='award-1'>
+          <div className='award-left-side' >
+          <h2>Rising Star in Voice</h2>
+          <h4>Awarded by Inside Voice Podcast (VOICE Summit 2019)</h4>
+          </div>
+          <div className='award-right-side' >
+          <img  className='award-img-1' src={VoiceAwards} />
+          </div>
+        </div>
+        <div className='award-2'>2</div>
+        <div className='award-3'>3</div>
+        <div className='award-4'>4</div>
+        <div className='award-5'>5</div>
+        <div className='award-6'>6</div>
+      </div>
+    </div>
   );
 }
 
