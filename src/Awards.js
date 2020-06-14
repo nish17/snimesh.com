780bc1e738cf10912674c366e513ce757e14ea44
Nimesh Solanki <nimesh3536@gmail.com>
2020-06-14 19:37:28 +0530

    add new awards and its styles

--- src/Awards.js
+++ src/Awards.js
@@ -4,6 +4,8 @@ import VOICE from './assets/Voice.webp';
 import VoiceAwards from './assets/inside_voice_awards.png';
 import RisingStar from './assets/risingStar.png';
 import bixbyPD from './assets/bixbyPD.png';
+import devfest18 from './assets/DevFest2018.jpeg';
+import devfest19 from './assets/DevFest2019.jpg';
 
 const Awards = () => {
   return (
@@ -55,9 +57,58 @@ const Awards = () => {
             <img className='award-img-2' src={bixbyPD} />
           </div>
         </div>
-        <div className='award-3'>3</div>
-        <div className='award-4'>4</div>
-        <div className='award-5'>5</div>
+        <div className='award-3'>
+          <div className='award-left-side'>
+            <div className='award-title'>#1 VOICE Ambassador Globally</div>
+            <div className='award-program'>Voice Event Ambassador Program</div>
+            <div className='award-org'>Voice Summit 2019</div>
+          </div>
+          <div className='award-right-side'>
+            <img className='award-img-3' src={VOICE} />
+          </div>
+        </div>
+        <div className='award-4'>
+          <div className='award-left-side'>
+            <div className='award-title'>Tech-Speaker</div>
+            <div className='award-program'>DevFest Ahmedabad 2019</div>
+            <div className='award-org'>GDG Ahmedabad</div>
+            <div className='award-link'>
+              <div>
+                Link:{' '}
+                <a
+                  href='https://speakerdeck.com/nimeshs17/best-practices-of-vui-design'
+                  target='_blank'
+                >
+                  SpeakerDeck
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='award-right-side'>
+            <img className='award-img-4' src={devfest19} />
+          </div>
+        </div>
+        <div className='award-5'>
+          <div className='award-left-side'>
+            <div className='award-title'>Tech-Speaker</div>
+            <div className='award-program'>DevFest Ahmedabad 2018</div>
+            <div className='award-org'>GDG Ahmedabad</div>
+            <div className='award-link'>
+              <div>
+                Link:{' '}
+                <a
+                  href='https://speakerdeck.com/jaldeepasodariya/codelab-actions-on-google-devfestahm18'
+                  target='_blank'
+                >
+                  SpeakerDeck
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='award-right-side'>
+            <img className='award-img-4' src={devfest18} />
+          </div>
+        </div>
         <div className='award-6'>6</div>
       </div>
     </div>
