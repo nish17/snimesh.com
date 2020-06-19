b16c32526d7f0456b9f4bab10ecf75869a13ff5a
Nimesh Solanki <nimesh3536@gmail.com>
2020-06-19 15:50:33 +0530

    fix about me component and its styles

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,50 +1,46 @@
 import React from 'react';
-
-import Container from 'muicss/lib/react/container';
-import Row from 'muicss/lib/react/row';
-import Col from 'muicss/lib/react/col';
-// import { Container, Row, Col } from 'react-bootstrap';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row'
-// import Col from 'react-bootstrap/Col'
-
 import ExperienceAndEducation from './ExperienceAndEducation';
 import GithubCard from './GithubCard';
 import StackOverFlowCard from './StackOverFlowCard';
 
 const AboutMe = () => {
   return (
-    <div>
-      <div className='grid-container'>
-        <div className='about-title-1'>
-          <h3>About</h3>
-        </div>
-        <div className='img'>
-          <img
-            className='my-pic-1'
-            src='https://avatars1.githubusercontent.com/u/12984120?v=4'
-          />
-        </div>
-        <div className='content-1'>
-          <p>
-            I'm a developer at heart with experience in{' '}
-            <a href='https://nodejs.org/'>Node.js</a> on the backend and{' '}
-            <a href='https://developers.google.com/assistant'>
-              Google Assistant
-            </a>{' '}
-            /<a href='https://developer.amazon.com/en-US/alexa'>Amazon Alexa</a>
-            /{' '}
-            <a href='https://www.samsung.com/us/explore/bixby/'>
-              Samsung Bixby
-            </a>{' '}
-            on the VoiceEnd. <br /> I am seeking for opportunity with a
-            competitive and challenging enivronment where I can serve your
-            organization and establish an enjoyable career for myself.
-          </p>
-        </div>
-        <div className='button-1'>
-          {/* <a href='mailto:nimesh3536@gmail.com'>Email me</a> */}
-          <a href='#Resume'>Checkout Resume</a>
+    <div className='aboutMe-container'>
+      <div className='aboutMe'>
+        <div className='grid-container'>
+          <div className='about-title-1'>
+            <h3>About</h3>
+          </div>
+          <div className='img'>
+            <img
+              className='my-pic-1'
+              src='https://avatars1.githubusercontent.com/u/12984120?v=4'
+            />
+          </div>
+          <div className='content-1'>
+            <p>
+              I'm a developer at heart with experience in{' '}
+              <a href='https://nodejs.org/'>Node.js</a> on the backend and{' '}
+              <a href='https://developers.google.com/assistant'>
+                Google Assistant
+              </a>{' '}
+              /
+              <a href='https://developer.amazon.com/en-US/alexa'>
+                Amazon Alexa
+              </a>
+              /{' '}
+              <a href='https://www.samsung.com/us/explore/bixby/'>
+                Samsung Bixby
+              </a>{' '}
+              on the VoiceEnd. <br /> I am seeking for opportunity with a
+              competitive and challenging enivronment where I can serve your
+              organization and establish an enjoyable career for myself.
+            </p>
+          </div>
+          <div className='button-1'>
+            {/* <a href='mailto:nimesh3536@gmail.com'>Email me</a> */}
+            <a href='#Resume'>Checkout Resume</a>
+          </div>
         </div>
       </div>
       <div>
